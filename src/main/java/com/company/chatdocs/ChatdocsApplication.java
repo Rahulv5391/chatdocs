2eea3d99404b94543acb11d0b38ec743076adf9c
Rahul Verma <vermarahul72787@gmail.com>
2026-09-25 15:16:59 +0530

    Add Postgres extensions migration

--- src/main/java/com/company/chatdocs/ChatdocsApplication.java
+++ src/main/java/com/company/chatdocs/ChatdocsApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatdocsApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ChatdocsApplication.class, args);
-
-		System.out.println("Hello");
-
 	}
 
 }
